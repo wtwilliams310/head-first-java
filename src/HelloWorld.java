88672be8075cdfb92bb16d7c864d7235c96eb11f
Titus Williams <wtwilliams310@gmail.com>
2023-02-13 09:04:23 -0600

    updated files

---
+++ src/HelloWorld.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args){
+//       char vowels[] = new char[5];
+
+//        vowels[0] = 'a';
+//        vowels[1] = 'e';
+//        vowels[2] = 'i';
+//        vowels[3] = 'o';
+//        vowels[4] = 'u';
+
+
+        // Sort and Array; sort by index
+//        char vowels[] = {'e', 'e', 'a', 'o', 'i'};
+//
+//        int startingIndex = 1;
+//        int endingIndex = 4;
+//
+//        Arrays.sort(vowels, startingIndex, endingIndex);
+//
+//        System.out.println(Arrays.toString(vowels));
+
+        // Search within an array (only works on a sorted array)
+//        char vowels[] = {'u', 'e', 'a', 'o', 'i'};
+//        Arrays.sort(vowels);
+//
+//        char key = 'o';
+//
+//        int foundItemIndex = Arrays.binarySearch(vowels, key);
+//
+//        System.out.println(Arrays.toString(vowels));
+//        System.out.println(foundItemIndex);
+
+        // Fill method
+        char vowels[] = {'u', 'e', 'a', 'o', 'i'};
+
+        // fill with all 'x'
+//        Arrays.fill(vowels, 'x');
+//        System.out.println(Arrays.toString(vowels));
+//
+        // fill bases on start and ending index
+
+//        int startingIndex = 1;
+//        int endingIndex = 4;
+//        Arrays.fill(vowels, startingIndex, endingIndex, 'x');
+//        System.out.println(Arrays.toString(vowels));
+
+        // Copy an Array
+
+    }
+}
